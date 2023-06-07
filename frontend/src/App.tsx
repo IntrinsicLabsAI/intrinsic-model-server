f8c35306d64615f7aefeea0afd547b406f8e6668
Tarrek Shaban <tarrek.shaban@gmail.com>
2023-06-07 16:52:21 -0400

    Added RTK and working query, removed CORs for DEV

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { useGetModelsQuery } from './services/baseService'
+
 export default function App() {
+
+  let { data, error, isLoading } = useGetModelsQuery('')
+  if(error) console.log(error)
+  if(!isLoading && data) console.log(data)
+
   return (
     <>
       <p className="text-3xl font-bold text-center mt-6">Intrinsic Model Server</p>
+      
     </>
   )
 };

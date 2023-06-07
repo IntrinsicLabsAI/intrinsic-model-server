f3a4f1cf23e672a9a3bf8bae53a626e2aa7ebdec
Tarrek Shaban <tarrek.shaban@gmail.com>
2023-06-07 15:40:13 -0400

    Fixed Tailwinds which was not working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,7 @@
-import { useState } from 'react'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="text-3xl font-bold text-center mt-6">Intrinsic Model Server</p>
     </>
   )
-}
-
-export default App;
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {

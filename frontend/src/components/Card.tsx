400d0fb7f036444ca322dde4919976ba25e34a7c
Tarrek Shaban <tarrek.shaban@gmail.com>
2023-06-09 08:00:01 -0400

    Minor UX changes

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -12,13 +12,13 @@ export default function Card({
     deleteHandler: (arg: string) => void
 }) {
   return (
-    <div className=" container outline rounded-md p-4 cursor-pointer hover:outline-blue-500">
+    <div className=" container outline rounded-md p-4 ">
         <div className="flex flex-col h-20 gap-2">
             <div className="flex flex-row items-center leading-tight">
                 <p className=" text-lg font-semibold ">{title}</p>
                 <TrashIcon 
                     onClick={() => deleteHandler(guid)}
-                    className="h-5 w-5 ml-auto hover:text-red-500 text-slate-500"/>
+                    className="cursor-pointer h-5 w-5 ml-auto hover:text-red-500 text-slate-500"/>
             </div>
             <div className="flex flex-row">
                 <p className=" font-mono text-sm"><span className=" font-sans font-semibold ">Version: </span>{version}</p>

dad7a624b82c1a2e9443a4589909538c1d81dbe8
Tarrek Shaban <tarrek.shaban@gmail.com>
2023-06-10 18:41:45 -0400

    Added ReactRouter to the project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './App.tsx'
 import { store } from './store'
-
 import './styles/index.css'
 
+import App from './App.tsx'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-      <Provider store={store}>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </Provider>
-    </React.StrictMode>,
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
---
+++ frontend/src/pages/Workspace.tsx
@@ -0,0 +1,10 @@
+// import { Routes, Route, Navigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+
+export default function Workspace() {
+    return (
+        <div>
+            <Outlet />
+        </div>
+    )
+};

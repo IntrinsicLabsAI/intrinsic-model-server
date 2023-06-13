60529158dbf43b933a11b1561007a44dc81b1edd
Andrew Duffy <andreweduffy@gmail.com>
2023-06-12 20:45:35 -0400

    add fix/check modes for script

--- scripts/pycheck.sh
+++ scripts/pycheck.sh
@@ -3,8 +3,22 @@
 set -eu -o pipefail
 
 rootdir=$(cd `dirname $0`/.. && pwd)
+mode=${FIX:-check}
 
-./venv/bin/isort --profile black --check --diff modelserver
-./venv/bin/black modelserver --check --diff
-./venv/bin/mypy --strict modelserver
-./venv/bin/pytest
+function check() {
+  ./venv/bin/isort --profile black --check --diff modelserver
+  ./venv/bin/black modelserver --check --diff
+  ./venv/bin/mypy --strict modelserver
+  ./venv/bin/pytest
+}
+
+function fix() {
+  ./venv/bin/isort --profile black modelserver
+  ./venv/bin/black modelserver
+}
+
+if [[ "$mode" = "check" ]]; then
+	check
+else
+	fix
+fi

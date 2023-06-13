8d2c375a102966ff9ff929fd1e0980abdf6c05b0
Andrew Duffy <andreweduffy@gmail.com>
2023-06-12 20:37:16 -0400

    pycheck harder?

--- scripts/pycheck.sh
+++ scripts/pycheck.sh
@@ -4,7 +4,10 @@ set -eu -o pipefail
 
 rootdir=$(cd `dirname $0`/.. && pwd)
 
-# $rootdir/venv/bin/isort --profile black --check --diff modelserver
-$rootdir/venv/bin/black modelserver --check --diff
-$rootdir/venv/bin/mypy --strict modelserver
-$rootdir/venv/bin/pytest
+cd $rootdir
+ls -l $rootdir
+
+./venv/bin/isort --profile black --check --diff modelserver
+./venv/bin/black modelserver --check --diff
+./venv/bin/mypy --strict modelserver
+./venv/bin/pytest

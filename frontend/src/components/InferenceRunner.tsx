55360149c8170cddd5523ecc3ab126c6a16b8221
Andrew Duffy <a10y@users.noreply.github.com>
2023-06-13 14:34:06 -0400

    Tarrek's InferenceRunner (#33)
    Add back inferencerunner

--- frontend/src/components/InferenceRunner.tsx
+++ frontend/src/components/InferenceRunner.tsx
@@ -1,6 +1,78 @@
+import { useState } from "react"
+import Dropdown from "./core/Dropdown"
+import DropdownItem from "./core/DropdownItem"
+import Widget from "./core/Widget"
+import { Icon } from "@blueprintjs/core";
+
 export default function InferenceRunner() {
+    const [isRunning, setIsRunning] = useState(false);
+
     return (
-        <div>
-        </div>
+        <Widget title="Inference Runner">
+            <div>
+                {!isRunning ? (
+                    <div className="flex flex-col w-full gap-4">
+                        <div className="flex flex-row w-full gap-4 justify-start">
+                            <h3 className=" text-gray-200">
+                                Run, iterate, and refine your prompts before using in production.
+                                All prompts are run against this WebServer.
+                                If responses are slow, try adding more resources to your server.</h3>
+                        </div>
+
+                        <div className="flex flex-row w-full gap-4 justify-start">
+                            <h3 className=" font-semibold leading-none">Prompt</h3>
+                            <textarea className="ml-auto w-[80%] bg-dark-200 focus:border-primary-100 focus:ring-0 focus:shadow-none rounded-sm h-40 text-gray-400" id="query" placeholder="Enter a query..." />
+                        </div>
+
+                        <div className="flex flex-row w-full gap-4 justify-start">
+                            <h3 className=" flex-grow font-semibold leading-none">Version</h3>
+                            <div className=" items-baseline w-[80%]">
+                                <Dropdown buttonText="Model Version">
+                                    <DropdownItem name="0.1.0" />
+                                    <DropdownItem name="0.2.0" />
+                                    <DropdownItem name="0.3.0" />
+                                </Dropdown>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row w-full gap-4 justify-start">
+                            <h3 className=" font-semibold leading-none">Tempurature</h3>
+                            <input className="ml-auto w-[80%] focus:border-primary-100 focus:ring-0 focus:shadow-none rounded bg-dark-200 text-gray-400" type="number" id="temp" />
+                        </div>
+
+                        <div className="flex flex-row w-full gap-4 justify-start">
+                            <h3 className=" font-semibold leading-none">Token Limit</h3>
+                            <input className="ml-auto w-[80%] focus:border-primary-100 focus:ring-0 focus:shadow-none rounded bg-dark-200 text-gray-400" type="number" id="token" />
+                        </div>
+
+                        <div className="flex flex-row w-full gap-4 justify-start">
+                            <button className="px-3 py-1.5 bg-primary-100 rounded hover:bg-primary-500 ml-auto" onClick={() => setIsRunning(true)}>
+                                <p className=" text-dark-300 font-semibold ">Run Inference</p>
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <>
+                    <div className="flex flex-col gap-2 w-full p-4 rounded bg-dark-200">
+                        <div className=" flex flex-row ">
+                            <h3 className=" font-semibold">Running Compleation Query</h3>
+                            <div className="ml-auto cursor-pointer p-1 hover:bg-primary-100/20 hover:rounded" onClick={() => setIsRunning(false)}><Icon icon="cross" color="#F6F7F9" /></div>
+                        </div>
+                        <h3 className=" font-mono text-sm ">
+                            This is the query a user entered. This will show the information that a user entered previously. This is the prompt. This is the query a user entered. This will show the information that a user entered previously. This is the prompt.
+                        </h3>
+                        <div className=" flex flex-row gap-3">
+                            <p className=" text-xs font-bold px-1.5 py-1 bg-primary-100 rounded text-dark-400"> Version: 0.1.0 </p>
+                            <p className=" text-xs font-bold px-1.5 py-1 bg-primary-100 rounded text-dark-400"> Temperature: 0.12 </p>
+                            <p className=" text-xs font-bold px-1.5 py-1 bg-primary-100 rounded text-dark-400"> Tokens: 128 </p>
+                        </div>
+                    </div>
+                    <div className="mt-4">
+                        <p>Put results here...</p>
+                    </div>
+                    </>
+                )}
+            </div>
+        </Widget>
     )
 }

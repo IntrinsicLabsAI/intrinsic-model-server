1c14efb593289d77255f4cbabee82638f0c8b5e4
Andrew Duffy <andreweduffy@gmail.com>
2023-06-12 20:26:06 -0400

    blah?

--- scripts/pycheck.sh
+++ scripts/pycheck.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 rootdir=$(cd `dirname $0`/.. && pwd)
 
-$rootdir/venv/bin/isort --profile black --check --diff modelserver
+# $rootdir/venv/bin/isort --profile black --check --diff modelserver
 $rootdir/venv/bin/black modelserver --check --diff
 $rootdir/venv/bin/mypy --strict modelserver
 $rootdir/venv/bin/pytest

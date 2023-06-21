fb4205fac1152b4e5e27087ee7108bb038174dbd
Tarrek Shaban <tarrek.shaban@gmail.com>
2023-06-20 20:25:12 -0400

    Baseline layout + ButtonInput Component

---
+++ frontend/src/components/form/ButtonInput.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+
+interface selectionButtonInfo {
+    title: string,
+    description: string,
+    value: string,
+}
+
+function SelectionButton(
+    {
+        title,
+        description,
+        value,
+        state,
+        setState
+    } : {
+        title: string,
+        description: string,
+        value: string,
+        state: string,
+        setState: (newState: string) => void,
+    }
+) {
+    return (
+        <div 
+            key={value}
+            className={`${state == value ? 
+                " bg-primary-100 p-4 outline outline-primary-600 rounded cursor-pointer" : 
+                "p-4 outline outline-slate-400 rounded cursor-pointer"}`}
+            onClick={() => setState(value)}>
+                <h3 className={`leading-none text-lg font-semibold ${state == value ? " text-dark-200 "  : "text-slate-200"} `}>{title}</h3>
+                <p className={` leading-tight pt-1 ${state == value ? " text-dark-200 "  : "text-slate-200"}`}>{description}</p>
+        </div>
+    )
+}
+
+export default function ButtonInput(
+    {
+        setState,
+        options
+    } : {
+        setState: (newState: string) => void,
+        options: selectionButtonInfo[],
+    }
+) {
+    const [selection, setSelection] = useState("")
+
+    const changeSelection = (newSelection: string) => {
+        setSelection(newSelection)
+        setState(newSelection)
+    }
+
+    return (
+        <div className=" grid grid-cols-2 gap-4">
+            {options.map((option) => (
+            <SelectionButton 
+                value={option.value} 
+                title={option.title}
+                description={option.description}
+                state={selection} 
+                setState={changeSelection}/>
+            ))}
+        </div>
+    )
+}
--- frontend/src/pages/NewModel.tsx
+++ frontend/src/pages/NewModel.tsx
@@ -7,6 +7,7 @@ import { ModelInfo, ModelType } from '../api'
 import Page from "../components/layout/Page"
 import OneColumnLayout from "../components/layout/OneColumnLayout"
 import TextInput from "../components/form/TextInput"
+import ButtonInput from "../components/form/ButtonInput"
 
 export default function NewModel() {
     const [name, setName] = useState("");
@@ -14,6 +15,7 @@ export default function NewModel() {
     const [modelType, /* setModelType */] = useState<ModelType>(ModelType.COMPLETION);
     const [modelPath, setModelPath] = useState<string>("");
     const [registerModelAction] = useRegisterModelMutation();
+    const [selection, setSelection] = useState("none");
 
     const navigate = useNavigate();
 
@@ -21,6 +23,62 @@ export default function NewModel() {
         registerModelAction(modelInfo)
         navigate("/")
     };
+
+    const diskForm = (
+        <form>
+            <div className="flex flex-row pt-4">
+                <div className="w-1/3">
+                    <p className=" text-lg font-semibold ">Model Name</p>
+                </div>
+                <div className="w-2/3">
+                    <TextInput
+                        placeholder="Model Name"
+                        setState={setName}
+                        name="model-name" />
+                </div>
+            </div>
+            <div className="flex flex-row pt-4">
+                <div className="w-1/3">
+                    <p className=" text-lg font-semibold ">Model Version</p>
+                </div>
+                <div className="w-2/3">
+                    <TextInput
+                        placeholder="0.0.1"
+                        setState={setVersion}
+                        name="model-version" />
+                </div>
+            </div>
+            <div className="flex flex-row pt-4">
+                <div className="w-1/3">
+                    <p className=" text-lg font-semibold ">Model Path</p>
+                </div>
+                <div className="w-2/3">
+                    <TextInput
+                        placeholder="location/of/model.bin"
+                        setState={setModelPath}
+                        name="model-path" />
+                </div>
+            </div>
+            <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-primary-400 hover:bg-primary-600 px-3 py-2 text-sm font-semibold text-dark-400 shadow-sm sm:ml-3 sm:w-auto"
+                    // disabled={!!name || !!modelPath || !!modelType}
+                    onClick={() => {
+                        registerHandler({
+                            name: name,
+                            version: version || undefined,
+                            model_type: modelType,
+                            model_params: {
+                                model_path: modelPath,
+                            }
+                        })
+                    }}>
+                    Register New Model
+                </button>
+            </div>
+        </form>
+    )
 
     return (
         <Page>
@@ -31,66 +89,31 @@ export default function NewModel() {
                         A model represents a set of binaries that are deployed to the server.
                         Use this to register a new model with the server.
                         Once deployed, models can be run from the Web Portal or via API.
-                        Each model is independently versioned.
                     </p>
                     <hr className=" border-primary-100" />
                 </div>
 
-                <form>
-                    <div className="flex flex-row pt-4">
-                        <div className="w-1/3">
-                            <p className=" text-lg font-semibold ">Model Name</p>
-                        </div>
-                        <div className="w-2/3">
-                            <TextInput
-                                placeholder="Model Name"
-                                setState={setName}
-                                name="model-name" />
-                        </div>
-                    </div>
-                    <div className="flex flex-row pt-4">
-                        <div className="w-1/3">
-                            <p className=" text-lg font-semibold ">Model Version</p>
-                        </div>
-                        <div className="w-2/3">
-                            <TextInput
-                                placeholder="0.0.1"
-                                setState={setVersion}
-                                name="model-version" />
-                        </div>
-                    </div>
-                    <div className="flex flex-row pt-4">
-                        <div className="w-1/3">
-                            <p className=" text-lg font-semibold ">Model Path</p>
-                        </div>
-                        <div className="w-2/3">
-                            <TextInput
-                                placeholder="location/of/model.bin"
-                                setState={setModelPath}
-                                name="model-path" />
-                        </div>
-                    </div>
-                    <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                        <button
-                            type="button"
-                            className="inline-flex w-full justify-center rounded-md bg-primary-400 hover:bg-primary-600 px-3 py-2 text-sm font-semibold text-dark-400 shadow-sm sm:ml-3 sm:w-auto"
-                            // disabled={!!name || !!modelPath || !!modelType}
-                            onClick={() => {
-                                registerHandler({
-                                    name: name,
-                                    version: version || undefined,
-                                    model_type: modelType,
-                                    model_params: {
-                                        model_path: modelPath,
-                                    }
-                                })
-                            }}>
-                            Register New Model
-                        </button>
-                    </div>
-                </form>
-
-            </OneColumnLayout>
-        </Page>
+                <div className="mt-4">
+                    <ButtonInput
+                        options={[
+                            {
+                                title: "Import from Disk",
+                                description: "Deploy a new model to the Web Server that is already available on your disk.",
+                                value: "disk"
+                            },
+                            {
+                                title: "Import from HuggingFace",
+                                description: "Select a model from HuggingFace to deploy to your Web Server.",
+                                value: "hugging-face"
+                            },
+                        ]}
+                        setState={setSelection}
+                    />
+                </div>
+                {selection == "none" && (<div>None Selected</div>)}
+                {selection == "disk" && (<>{diskForm}</>)}
+                {selection == "hugging-face" && (<p>Hugging Face</p>)}
+        </OneColumnLayout>
+        </Page >
     )
 }

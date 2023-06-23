295ddee0ad5677b94f690a97bb1bb6d72fc92d9c
Andrew Duffy <andreweduffy@gmail.com>
2023-06-23 00:44:57 -0400

    more models to test with

--- frontend/src/data/endorsedModels.ts
+++ frontend/src/data/endorsedModels.ts
@@ -10,8 +10,18 @@ export const endorsedModels: Array<{ repo: string, name: string, description: st
         description: "Vicuna 13B model, with variable quantization methods",
     },
     {
+        repo: "TheBloke/Wizard-Vicuna-13B-Uncensored-GGML",
+        name: "Vicuna 13B Uncensored",
+        description: "Uncensored variant of Vicuna 13B 😈",
+    },
+    {
         repo: "TheBloke/WizardLM-Uncensored-Falcon-7B-GGML",
         name: "WizardLM Uncensored 7B",
         description: "Uncensored model trained by \"TheBloke\" based off of the Falcon 7B model",
+    },
+    {
+        repo: "TheBloke/falcon-7b-instruct-GGML",
+        name: "TheBloke Falcon 7B Instruct-tuned model",
+        description: "Uncensored instruction-tuned model",
     }
 ];

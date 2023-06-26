fadd46e70506fd4e2f7f665db9a21a21bfe3e903
Andrew Duffy <andreweduffy@gmail.com>
2023-06-26 11:04:41 -0400

    Populate model sidebar from info on the new APIs

--- frontend/src/pages/Model.tsx
+++ frontend/src/pages/Model.tsx
@@ -4,18 +4,47 @@ import { useParams } from "react-router-dom";
 import EditableCode from "../components/EditableCode";
 import Widget from "../components/core/Widget";
 import InferenceRunner from "../components/InferenceRunner";
-import { useGetDescriptionQuery, useUpdateDescriptionMutation } from "../api/services/v1";
+import { useGetDescriptionQuery, useGetModelsQuery, useUpdateDescriptionMutation, isHuggingFaceSource } from "../api/services/v1";
 
 import Page from "../components/layout/Page";
 import OneColumnLayout from "../components/layout/OneColumnLayout";
 import TwoColumnLayout from "../components/layout/TwoColumnLayout";
 import Column from "../components/layout/Column";
 import Pill from "../components/core/Pill";
+import { useMemo } from "react";
 
 export default function Model() {
     const { name } = useParams<"name">();
     // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
     const modelName = name!;
+
+    const { registeredModel } = useGetModelsQuery(undefined, {
+        selectFromResult: ({ data }) => ({ registeredModel: data?.models.find(m => m.name === modelName) })
+    })
+
+    const source = useMemo(() => {
+        if (registeredModel === undefined) {
+            return undefined;
+        }
+
+        const latest = registeredModel.versions[registeredModel.versions.length - 1];
+        const importSource = latest.import_metadata.source
+        if (isHuggingFaceSource(importSource)) {
+            return {
+                link: `https://huggingface.co/${importSource.source.repo}/blob/main/${importSource.source.file}`,
+                popover: importSource.source.repo,
+                importTime: latest.import_metadata.imported_at,
+                sourceDisplayName: "HuggingFace",
+            }
+        } else {
+            return {
+                link: undefined,
+                popover: importSource.source.path,
+                importTime: latest.import_metadata.imported_at,
+                sourceDisplayName: "File Upload",
+            }
+        }
+    }, [registeredModel]);
 
     const { data: description } = useGetDescriptionQuery(modelName);
     const [updateDescriptionAction] = useUpdateDescriptionMutation();
@@ -63,29 +92,29 @@ export default function Model() {
                     <InferenceRunner model={modelName} />
                 </Column>
                 <Column>
-                    <Widget title="About">
-                        <div className="overflow-y-auto [&::-webkit-scrollbar]:hidden">
+                    <Widget title="About">{
+                        registeredModel &&
+                        (<div className="overflow-y-auto [&::-webkit-scrollbar]:hidden">
                             <div className="flex flex-row items-center gap-4 pb-4" >
                                 <p className=" w-1/3 font-semibol ">Model Type</p>
-                                <Pill text="Completion"></Pill>
+                                <Pill text={registeredModel.model_type}></Pill>
                             </div>
                             <div className="flex flex-row items-center gap-4 pb-4" >
                                 <p className=" w-1/3 font-semibold">Source</p>
-                                <Pill text="HuggingFace"></Pill>
+                                <a href={source?.link ?? "about:blank"} target="_blank">
+                                    <Pill text={source?.sourceDisplayName ?? ""}></Pill>
+                                </a>
                             </div>
                             <div className="flex flex-row items-center gap-4 pb-4" >
                                 <p className=" w-1/3 font-semibold">Lineage</p>
-                                <Pill text="User Upload"></Pill>
+                                <Pill text="LLaMa"></Pill>
                             </div>
                             <div className="flex flex-row items-center gap-4 pb-4" >
                                 <p className=" w-1/3 font-semibold">Format</p>
-                                <Pill text="GGML"></Pill>
+                                <Pill text={registeredModel.runtime}></Pill>
                             </div>
-                            <div className="flex flex-row items-center gap-4 pb-4" >
-                                <p className=" w-1/3 font-semibold">Context Size</p>
-                                <Pill text="4,250 Tokens"></Pill>
-                            </div>
-                        </div>
+                        </div>)
+                    }
                     </Widget>
                     <Widget title="History">
                         <div className="overflow-y-auto [&::-webkit-scrollbar]:hidden">

5600ed8280210b1f8123477dfee601a03c61145d
Andrew Duffy <a10y@users.noreply.github.com>
2023-07-05 17:42:12 -0400

    Add Import button on homepage when you don't have any models (#106)
    **Before**
    
    At homepage you'd need to navigate to upper-right corner to click thru
    to the create new model menu
    
    ![Screenshot 2023-07-05 at 2 11 25
    PM](https://github.com/IntrinsicLabsAI/intrinsic-model-server/assets/332583/63792516-bed3-4ce2-9a50-11d90c32172b)
    
    
    **After**
    
    Import button embedded right in the homepage, making it one-click to
    import your first model
    
    
    ![Screenshot 2023-07-05 at 2 04 48
    PM](https://github.com/IntrinsicLabsAI/intrinsic-model-server/assets/332583/835ce688-f281-4038-a6ba-6ad75575aa91)

--- frontend/src/components/core/Button.tsx
+++ frontend/src/components/core/Button.tsx
@@ -1,33 +1,154 @@
 import { BlueprintIcons_16Id } from "@blueprintjs/icons/src/generated/16px/blueprint-icons-16.ts"
 import { Icon } from '@blueprintjs/core'
 
+
+/**
+* A versitile, basic button for use as part of IDS. Built on top of HTML button component for accessibility.
+* @component Button
+* @param {string} [buttonText] - The button text to display, no text is shown if none is provided.
+* @param {BlueprintIcons_16Id} [buttonIcon] - The Blueprint Icon to display, no icon is shown if none is provided.
+* @param {("small"|"medium"|"large")} [size="medium"] - The size of the button.
+* @param {function} [onAction] - The function to be called when the button is clicked, no function is run if none is provided.
+* @param {boolean} [disabled=false] - Determines if the button is disabled.
+* @param {boolean} [outline=true] - Determines if the button has an outline applied
+* @param {("default" | "primary" | "danger")} [color="default"] - The color scheme of the button.
+* @param {("minimal" | "bold")} [style="minimal"] - The style of the button.
+* @returns {JSX.Element} The rendered Button component.
+*/
+
 export default function Button(
     {
         buttonText,
         buttonIcon,
+        size,
+        style,
         onAction,
-        type,
+        color,
+        outline,
         disabled
     } : {
         buttonText?: string,
         buttonIcon?: BlueprintIcons_16Id,
+        size?: "small" | "medium" | "large",
+        style?: "minimal" | "bold"
         onAction?: () => void,
-        type: 'text' | 'icon',
-        disabled?: boolean
+        disabled?: boolean,
+        outline?: boolean,
+        color?: "default" | "primary" | "danger"
     }
 ) {
+    const outlineProp = (outline === undefined ? true : outline);
+    const colorProp = (color === undefined ? "default" : color);
+    const sizeProp = (size === undefined ? "medium" : size);
+    const disabledProp = (disabled === undefined ? false : disabled)
+    const styleProp = (style === undefined ? "minimal" : style)
+
+    const sizeSystem = {
+        small: {
+            icon: 12,
+            text: "font-medium text-sm",
+            gap: "gap-1",
+            padding: "px-1.5 py-1"
+        },
+        medium: {
+            icon: 16,
+            text: "font-medium text-base",
+            gap: "gap-2",
+            padding: "px-2.5 py-2"
+        },
+        large: {
+            icon: 20,
+            text: "font-medium text-lg",
+            gap: "gap-3",
+            padding: "px-3 py-2.5"
+        }
+    }
+
+    const colorSystem = {
+        minimal :{
+            default: {
+                outline: "outline outline-gray-400",
+                background: "",
+                text: "text-gray-400",
+                icon: "#F6F7F9",
+                hover: "hover:bg-slate-400/20"
+            },
+            primary: {
+                outline: "outline outline-primary-400",
+                background: "",
+                text: "text-primary-400",
+                icon: "#6cc0a6",
+                hover: "hover:bg-primary-400/10"
+            },
+            danger: {
+                outline: "outline outline-red-400",
+                background: "",
+                text: "text-red-400",
+                icon: "#f1616f",
+                hover: "hover:bg-red-400/10"
+            },
+            disabled: {
+                outline: "",
+                background: "bg-gray-200/20",
+                text: "text-gray-200",
+                icon: "#DCE0E5",
+                hover: ""
+            }
+        },
+        bold: {
+            default: {
+                outline: "",
+                background: "bg-gray-400",
+                text: "text-dark-400",
+                icon: "#383E47",
+                hover: "hover:bg-gray-400/80"
+            },
+            primary: {
+                outline: "",
+                background: "bg-primary-400",
+                text: "text-dark-400",
+                icon: "#383E47",
+                hover: "hover:bg-primary-400/80"
+            },
+            danger: {
+                outline: "",
+                background: "bg-red-400",
+                text: "text-dark-400",
+                icon: "#383E47",
+                hover: "hover:bg-red-400/80"
+            },
+            disabled: {
+                outline: "",
+                background: "bg-gray-200/20",
+                text: "text-gray-200",
+                icon: "#DCE0E5",
+                hover: ""
+            }
+        }
+    }
+
     return (
         <>
-            {type === 'icon' && (
-                <div onClick={onAction} className=' cursor-pointer flex flex-row gap-2 p-2 hover:bg-slate-400/20 rounded items-center'>
-                    {buttonIcon &&<Icon icon={buttonIcon} size={20} color={"#53b79a"} />}
-                    {buttonText && <p className='text-primary-600 leading-none font-medium'>{buttonText}</p>}
-                </div>
-            )}
-            {type === 'text' && (
-                <div onClick={onAction} className={` ${disabled ? "outline-gray-400/40 bg-gray-400/20 cursor-not-allowed" : "outline-primary-400 cursor-pointer"} h-fit outline flex flex-row gap-2 p-2 rounded items-center `}>
-                    {buttonIcon &&<Icon icon={buttonIcon} size={14} color={"#53b79a"} />}
-                    {buttonText && <button className={`${disabled ? "text-gray-400/60 cursor-not-allowed" : "text-primary-600"} leading-none`}>{buttonText}</button>}
+            {!disabledProp ? (
+                <div onClick={onAction} 
+                    className={` flex flex-row cursor-pointer rounded items-center
+                        ${sizeSystem[sizeProp]["padding"]}
+                        ${colorSystem[styleProp][colorProp]["background"]}
+                        ${sizeSystem[sizeProp]["gap"]}
+                        ${colorSystem[styleProp][colorProp]["hover"]}
+                        ${outlineProp ? `${colorSystem[styleProp][colorProp]["outline"]}` : ""}`}>
+                    {buttonIcon && <Icon icon={buttonIcon} size={sizeSystem[sizeProp]["icon"]} color={`${colorSystem[styleProp][colorProp]["icon"]}`} />}
+                    {buttonText && <p className={`leading-none ${sizeSystem[sizeProp]["text"]} ${colorSystem[styleProp][colorProp]["text"]}`}> {buttonText} </p>}
+                </div> 
+            ) : (
+                <div onClick={onAction} 
+                    className={` flex flex-row px-3 py-2 rounded items-center cursor-not-allowed
+                        ${colorSystem[styleProp]["disabled"]["background"]}
+                        ${sizeSystem[sizeProp]["gap"]}
+                        ${colorSystem[styleProp]["disabled"]["hover"]}
+                        ${outlineProp ? `${colorSystem[styleProp]["disabled"]["outline"]}` : ""}`}>
+                    {buttonIcon && <Icon icon={buttonIcon} size={sizeSystem[sizeProp]["icon"]} color={`${colorSystem[styleProp]["disabled"]["icon"]}`} />}
+                    {buttonText && <p className={`leading-none ${sizeSystem[sizeProp]["text"]} ${colorSystem[styleProp]["disabled"]["text"]}`}> {buttonText} </p>}
                 </div>
             )}
         </>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,10 +10,13 @@ import Column from '../components/layout/Column';
 import { featureUpdates } from '../data/featureUpdates';
 import Card from '../components/core/Card';
 import { Icon } from '@blueprintjs/core';
+import { useNavigate } from 'react-router';
+import Button from '../components/core/Button';
 
 export default function Home() {
   // All models
-  const { data, isLoading } = useGetModelsQuery()
+  const { data, isLoading } = useGetModelsQuery();
+  const navigate = useNavigate();
 
   return (
     <>
@@ -33,22 +36,19 @@ export default function Home() {
 
         <TwoColumnLayout type="left">
           <Column>
-            <Widget title="Registered Models">
-              <div className="flex flex-col h-full">
-                <div className="flex flex-row items-center w-full">
-                  <p className=' text-lg font-base leading-tight text-gray-200'>
-                    The models listed below are currently active and available for use.
-                    Different versions of each model can be used by indicating the version when invoking the model.
-                    You can register new models here or via API.
-                    For information how how to use the API, view the included <a className='underline underline-offset-2' href="http://127.0.0.1:8000/docs">documentation</a>.
-                  </p>
-                </div>
-
-                <div className="flex flex-row items-center w-full mt-4">
-                  <h3 className=' text-xl font-semibold'>Completion Models</h3>
-                </div>
-
-                {(data?.models.length ?? 0) > 0 ? (
+            {(data?.models.length ?? 0) > 0 ? (
+              <Widget title="Registered Models">
+                <div className="flex flex-col h-full">
+                  <div className="flex flex-row items-center w-full">
+                    <p className=' text-lg font-base leading-tight text-gray-200'>
+                      The models listed below are currently active and available for use with Intrinsic Server.
+                      Different versions of each model can be used by indicating the version when invoking the model.
+                      You can register new models using this App or via API.
+                    </p>
+                  </div>
+                  <div className="flex flex-row items-center w-full mt-4">
+                    <h3 className=' text-xl font-semibold'>Completion Models</h3>
+                  </div>
                   <div className="grid grid-cols-2 gap-4 mt-4 mb-4">
                     {(!isLoading && data) ? (
                       data.models.map((model) => (
@@ -61,42 +61,52 @@ export default function Home() {
                       null
                     )}
                   </div>
-                ) : (
-                  <div className="mt-2">
-                    <p className=''>
-                      No models were found.
-                      This could be because no models have been registered or the server is offline.
-                      To get started, please register a model.
-                    </p>
-                  </div>
-                )}
-
+                </div>
+              </Widget>
+          ) : (
+            <Card>
+              <h2 className=' text-xl font-semibold pb-2 '>Getting Started with Intrinsic Server</h2>
+              <p className=' leading-snug'>
+                You don't seem to have any models registered.
+                Import a model to start using Intrinsic Server.
+                You can import directly from HuggingFace or upload the model file from disk.
+                Check out the <span>documentation</span> if you have any questions.
+              </p>
+              <div className="mt-4 w-fit mx-auto">
+                <Button
+                  color="primary"
+                  size="large"
+                  style="bold"
+                  buttonIcon='cloud-upload'
+                  buttonText='Import a model'
+                  onAction={() => { navigate('/new-model') }} />
               </div>
-            </Widget>
-          </Column>
+            </Card>
+          )}
+        </Column>
 
-          <Column>
-            <Widget title="Feature Updates">
-              <div>
-                {featureUpdates.sort((a, b) => b.id - a.id).map((update) => (
-                  <div className=" pb-2 " key={update.id}>
-                    <Card>
-                      <div className="flex flex-col w-full gap-1">
-                        <h3 className=' text-lg font-medium '>{update.title}</h3>
-                        <div className='flex flex-row gap-1.5 items-center'>
-                          <Icon icon="time" size={14} color="#82BEC7" />
-                          <p className=' text-sm font-medium leading-none text-blue-500'>{update.date.toDateString()}</p>
-                        </div>
-                        <p className=' pt-2 whitespace-pre-wrap '>{update.description}</p>
+        <Column>
+          <Widget title="Feature Updates">
+            <div>
+              {featureUpdates.sort((a, b) => b.id - a.id).map((update) => (
+                <div className=" pb-2 " key={update.id}>
+                  <Card>
+                    <div className="flex flex-col w-full gap-1">
+                      <h3 className=' text-lg font-medium '>{update.title}</h3>
+                      <div className='flex flex-row gap-1.5 items-center'>
+                        <Icon icon="time" size={14} color="#82BEC7" />
+                        <p className=' text-sm font-medium leading-none text-blue-500'>{update.date.toDateString()}</p>
                       </div>
-                    </Card>
-                  </div>
-                  ))}
-              </div>
-            </Widget>
-          </Column>
-        </TwoColumnLayout >
-      </Page >
+                      <p className=' pt-2 whitespace-pre-wrap '>{update.description}</p>
+                    </div>
+                  </Card>
+                </div>
+              ))}
+            </div>
+          </Widget>
+        </Column>
+      </TwoColumnLayout >
+    </Page >
     </>
   )
 }

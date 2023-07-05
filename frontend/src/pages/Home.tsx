df307d24c34532797142cbe034d68395d33a5dae
Tarrek Shaban <tarrek.shaban@gmail.com>
2023-07-05 16:13:30 -0400

    Updated style for Getting Started

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -36,22 +36,19 @@ export default function Home() {
 
         <TwoColumnLayout type="left">
           <Column>
-            <Widget title="Registered Models">
-              <div className="flex flex-col h-full">
-                <div className="flex flex-row items-center w-full">
-                  <p className=' text-lg font-base leading-tight text-gray-200'>
-                    The models listed below are currently active and available for use.
-                    Different versions of each model can be used by indicating the version when invoking the model.
-                    You can register new models here or via API.
-                    For information how how to use the API, view the included <a className='underline underline-offset-2' href="http://127.0.0.1:8000/docs">documentation</a>.
-                  </p>
-                </div>
-
-                <div className="flex flex-row items-center w-full mt-4">
-                  <h3 className=' text-xl font-semibold'>Completion Models</h3>
-                </div>
-
-                {(data?.models.length ?? 0) > 0 ? (
+            {(data?.models.length ?? 0) > 0 ? (
+              <Widget title="Registered Models">
+                <div className="flex flex-col h-full">
+                  <div className="flex flex-row items-center w-full">
+                    <p className=' text-lg font-base leading-tight text-gray-200'>
+                      The models listed below are currently active and available for use with Intrinsic Server.
+                      Different versions of each model can be used by indicating the version when invoking the model.
+                      You can register new models using this App or via API.
+                    </p>
+                  </div>
+                  <div className="flex flex-row items-center w-full mt-4">
+                    <h3 className=' text-xl font-semibold'>Completion Models</h3>
+                  </div>
                   <div className="grid grid-cols-2 gap-4 mt-4 mb-4">
                     {(!isLoading && data) ? (
                       data.models.map((model) => (
@@ -64,48 +61,50 @@ export default function Home() {
                       null
                     )}
                   </div>
-                ) : (
-                  <div className="mt-2 container">
-                    <p>
-                      You don't seem to have any models!
-                      To get started, please import a model file using either an integrated repository or a file.
-                    </p>
-                    <div className="mt-4 w-fit mx-auto">
-                      <Button 
-                        size="large"
-                        buttonIcon='cloud-upload' 
-                        buttonText='Import a model' 
-                        onAction={() => {navigate('/new-model')}} />
-                    </div>
-                  </div>
-                )}
-
+                </div>
+              </Widget>
+          ) : (
+            <Card>
+              <h2 className=' text-xl font-semibold pb-2 '>Getting Started with Your First Model</h2>
+              <p className=' leading-snug'>
+                You don't seem to have any models registered.
+                Import a model file to start using Intrinsic Server. 
+                You can import directly from HuggingFace or upload a model file from disk.
+                Check out the <span>documentation</span> if you have any questions.
+              </p>
+              <div className="mt-4 w-fit mx-auto">
+                <Button
+                  size="large"
+                  buttonIcon='cloud-upload'
+                  buttonText='Import a model'
+                  onAction={() => { navigate('/new-model') }} />
               </div>
-            </Widget>
-          </Column>
+            </Card>
+          )}
+        </Column>
 
-          <Column>
-            <Widget title="Feature Updates">
-              <div>
-                {featureUpdates.sort((a, b) => b.id - a.id).map((update) => (
-                  <div className=" pb-2 " key={update.id}>
-                    <Card>
-                      <div className="flex flex-col w-full gap-1">
-                        <h3 className=' text-lg font-medium '>{update.title}</h3>
-                        <div className='flex flex-row gap-1.5 items-center'>
-                          <Icon icon="time" size={14} color="#82BEC7" />
-                          <p className=' text-sm font-medium leading-none text-blue-500'>{update.date.toDateString()}</p>
-                        </div>
-                        <p className=' pt-2 whitespace-pre-wrap '>{update.description}</p>
+        <Column>
+          <Widget title="Feature Updates">
+            <div>
+              {featureUpdates.sort((a, b) => b.id - a.id).map((update) => (
+                <div className=" pb-2 " key={update.id}>
+                  <Card>
+                    <div className="flex flex-col w-full gap-1">
+                      <h3 className=' text-lg font-medium '>{update.title}</h3>
+                      <div className='flex flex-row gap-1.5 items-center'>
+                        <Icon icon="time" size={14} color="#82BEC7" />
+                        <p className=' text-sm font-medium leading-none text-blue-500'>{update.date.toDateString()}</p>
                       </div>
-                    </Card>
-                  </div>
-                ))}
-              </div>
-            </Widget>
-          </Column>
-        </TwoColumnLayout >
-      </Page >
+                      <p className=' pt-2 whitespace-pre-wrap '>{update.description}</p>
+                    </div>
+                  </Card>
+                </div>
+              ))}
+            </div>
+          </Widget>
+        </Column>
+      </TwoColumnLayout >
+    </Page >
     </>
   )
 }

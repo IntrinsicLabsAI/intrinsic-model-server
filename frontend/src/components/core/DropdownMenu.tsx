8c2dee1552fd373853344870d1c56f9c1ef6d82d
Tarrek Shaban <tarrek.shaban@gmail.com>
2023-07-05 17:33:07 -0400

    Migrate DropdownMenu to using Button

--- frontend/src/components/core/DropdownMenu.tsx
+++ frontend/src/components/core/DropdownMenu.tsx
@@ -1,11 +1,11 @@
 import { Menu } from '@headlessui/react'
 import React from 'react'
+import Button from './Button'
 
 import { BlueprintIcons_16Id } from "@blueprintjs/icons/src/generated/16px/blueprint-icons-16.ts"
 import { Icon } from '@blueprintjs/core'
 
 export default function DropdownMenu<K extends React.Key, T extends { id: K, value: string, icon?: BlueprintIcons_16Id  }>({
-    type,
     buttonText,
     buttonIcon,
     items,
@@ -24,18 +24,7 @@ export default function DropdownMenu<K extends React.Key, T extends { id: K, val
         <Menu as="div" className="relative inline-block text-left">
             <div>
                 <Menu.Button>
-                    {type === 'text' && (
-                        <div className='flex flex-row items-center gap-2 outline outline-primary-600 px-2 py-1 rounded'>
-                            {buttonIcon && <Icon icon={buttonIcon} size={16} color={"#53b79a"} />}
-                            <p className='text-primary-600 font-semibold'>{buttonText}</p>
-                        </div>
-                    )}
-                    {type === 'icon' && (
-                        <div className='flex flex-row gap-2 p-2 hover:bg-slate-400/20 rounded items-center'>
-                            <Icon icon={buttonIcon} size={20} color={"#53b79a"} />
-                            {buttonText && <p className='text-primary-600 leading-none font-medium'>{buttonText}</p>}
-                        </div>
-                    )}
+                    <Button size="large" outline={false} buttonText={buttonText} buttonIcon={buttonIcon}/>
                 </Menu.Button>
             </div>
 

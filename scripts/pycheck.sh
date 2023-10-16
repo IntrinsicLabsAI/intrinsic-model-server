dc373f99f606c36cd3a7868af88edb8cf1179f29
Andrew Duffy <andreweduffy@gmail.com>
2023-10-16 17:27:36 -0400

    add depds for pycheck

--- scripts/pycheck.sh
+++ scripts/pycheck.sh
@@ -6,16 +6,16 @@ rootdir=$(cd `dirname $0`/.. && pwd)
 mode=${FIX:-check}
 
 function check() {
-  ./venv/bin/isort --profile black --check --diff modelserver
-  ./venv/bin/black modelserver --check --diff
-  ./venv/bin/mypy --strict modelserver --ignore-missing-imports
-  ./venv/bin/pytest -v
-  ./venv/bin/ruff check modelserver
+  .venv/bin/isort --profile black --check --diff modelserver
+  .venv/bin/black modelserver --check --diff
+  .venv/bin/mypy --strict modelserver --ignore-missing-imports
+  .venv/bin/pytest -v
+  .venv/bin/ruff check modelserver
 }
 
 function fix() {
-  ./venv/bin/isort --profile black modelserver
-  ./venv/bin/black modelserver
+  .venv/bin/isort --profile black modelserver
+  .venv/bin/black modelserver
 }
 
 if [[ "$mode" = "check" ]]; then

aaca461a69710559884c6d4ea507dac4b088a2b1
Andrew Duffy <a10y@users.noreply.github.com>
2023-10-19 17:14:37 -0400

    Worker impl (#201)
    Extremely basic, just the bare bones
    
    * Fix some bugs in #199 
    * Added worker process, ideally run via a Docker image or some other
    hermetic dependency seal
    
    See brief screenrecording below (might wanna watch at 2x speed)
    
    ## FLUPs
    
    - [ ] Add support for S3 files
    - [ ] In-line conversion of FineTune outputs back to GGML for use with
    baseline GGML models
    - [ ] Live streaming of logs for faster debugging
    
    
    https://intrinsiclabs-my.sharepoint.com/:v:/g/personal/andrew_intrinsiclabs_ai/EYIHu20QQhVDi5DRW8-79ZkBokKWUOXZDCDIEspQsCKEPA?e=E2eBQb&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZyIsInJlZmVycmFsQXBwUGxhdGZvcm0iOiJXZWIiLCJyZWZlcnJhbE1vZGUiOiJ2aWV3In19

--- modelserver/db/remoteworker.py
+++ modelserver/db/remoteworker.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from abc import ABC, abstractmethod
 from datetime import datetime
@@ -10,10 +11,13 @@ from starlette.status import HTTP_404_NOT_FOUND
 
 from modelserver.types.remoteworker import (
     FineTuneJobIn,
+    FineTuneJobOut,
     JobState,
     RemoteWorkerDetailsIn,
     RemoteWorkerDetailsOut,
 )
+
+logger = logging.getLogger(__name__)
 
 
 class RemoteWorkerStore(ABC):
@@ -46,9 +50,21 @@ class RemoteWorkerStore(ABC):
         """
 
     @abstractmethod
-    def job_states(self) -> dict[UUID4, JobState]:
+    def get_jobs(self) -> list[FineTuneJobOut]:
         """
         Return the current status of all jobs tracked by the RemoteWorker system.
+        """
+
+    @abstractmethod
+    def get_assigned_jobs(self, worker_id: str) -> list[FineTuneJobOut]:
+        """
+        Returns a set of fine-tune jobs assigned to worker
+        """
+
+    @abstractmethod
+    def update_job_state(self, job_id: UUID4, job_state: JobState) -> None:
+        """
+        Update the job state. Called by RemoteWorker to report updated status.
         """
 
 
@@ -56,8 +72,7 @@ class RemoteWorkerStore(ABC):
 class InMemoryRemoteWorkerStore(RemoteWorkerStore):
     def __init__(self) -> None:
         self.workers: dict[str, RemoteWorkerDetailsOut] = {}
-        self.jobs: dict[UUID4, JobState] = {}
-        self.job_assignments: dict[UUID4, str] = {}
+        self.jobs: dict[UUID4, FineTuneJobOut] = {}
 
     def update_worker_details(self, worker_details: RemoteWorkerDetailsIn) -> None:
         now = datetime.utcnow()
@@ -72,28 +87,70 @@ class InMemoryRemoteWorkerStore(RemoteWorkerStore):
             registered_at=registered_at,
             last_reported=now,
         )
+
+        logger.info("registering worker %s", worker_details)
         self.workers[worker_details.name] = worker_details_saved
+
+        # Assign any unassigned jobs to this worker
+        for job in self.jobs.values():
+            if job.assigned_worker is None:
+                logger.info(
+                    "Assigning job %s to worker %s",
+                    job.id,
+                    worker_details.name,
+                )
+                job.assigned_worker = worker_details.name
 
     def get_workers(self) -> list[RemoteWorkerDetailsOut]:
         return list(self.workers.values())
 
     def submit_finetunejob(self, job: FineTuneJobIn) -> UUID4:
         job_id = uuid4()
-        self.jobs[job_id] = JobState.QUEUED
+        saved_job = FineTuneJobOut(
+            **job.model_dump(),
+            id=job_id,
+            submitted_at=datetime.utcnow(),
+        )
 
-        # try to assign worker if any are currently registered
+        # Assign to random worker if any are registered.
         if len(self.workers) > 0:
-            assigned_worker = random.choice(list(self.workers.values()))
-            self.job_assignments[job_id] = assigned_worker.name
-            self.jobs[job_id] = JobState.SCHEDULED
+            assigned_worker = random.choice(list(self.workers.values())).name
+            logger.info("assigning job %s to worker %s", saved_job.id, assigned_worker)
+            saved_job.assigned_worker = assigned_worker
+            saved_job.state = JobState.SCHEDULED
+        else:
+            logger.info("Will assign job in the future when worker is available")
+
+        self.jobs[job_id] = saved_job
 
         return job_id
 
     def job_state(self, job_id: UUID4) -> JobState:
-        state = self.jobs.get(job_id)
-        if state is None:
-            raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No such job")
-        return state
+        job = self.jobs.get(job_id)
+        if job is None:
+            raise HTTPException(
+                status_code=HTTP_404_NOT_FOUND, detail=f"No such job {job_id}"
+            )
+        return job.state
 
-    def job_states(self) -> dict[UUID4, JobState]:
-        return self.jobs.copy()
+    def get_jobs(self) -> list[FineTuneJobOut]:
+        return list(self.jobs.values())
+
+    def get_assigned_jobs(self, worker_id: str) -> list[FineTuneJobOut]:
+        """
+        Returns a set of fine-tune jobs assigned to worker
+        """
+        return [job for job in self.jobs.values() if job.assigned_worker == worker_id]
+
+    def update_job_state(self, job_id: UUID4, job_state: JobState) -> None:
+        """
+        Update the job state. Called by RemoteWorker to report updated status.
+        """
+
+        if job_id not in self.jobs:
+            raise HTTPException(
+                status_code=HTTP_404_NOT_FOUND, detail=f"No such job {job_id}"
+            )
+        self.jobs[job_id].state = job_state
+
+        # Enums are not in fact sorted in that way, unfortunately.
